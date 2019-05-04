24088c0d07a3a0f5d1f8face03208f61f28246ed
daudmu21 <daudmuhudin@gmail.com>
2019-05-04 14:40:32 -0500

    add

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,16 +1,15 @@
 node {
     properties([parameters([string(defaultValue: 'IP', description: 'where to build e.g IP', name: 'ENV', trim: true)])])
-    stage("Clone repo")
-    git "git@github.com:daudmu21/flask-examples.git"
+    stage("Clone repo"){
+      git "git@github.com:daudmu21/flask-examples.git"
     }
     stage("Install Requirements"){
-    sh "scp -r *  ec2-user@${ENV}:/tmp"
-    sh "ssh ec2-user@${ENV} virtualenv /tmp/venv"
-    sh "ssh ec2-user@${ENV} ./tmp/ven/activate"
-    sh "ssh ec2-user@${ENV} sudo pip install -r /tmp/requirements.txt"
-    }
+        sh "scp -r *  ec2-user@${ENV}:/tmp"
+        sh "ssh ec2-user@${ENV} virtualenv /tmp/venv"
+        sh "ssh ec2-user@${ENV} ./tmp/ven/activate"
+        sh "ssh ec2-user@${ENV} sudo pip install -r /tmp/requirements.txt"
+     }
     stage("start python app"){
         sh "ssh ec2-user@${ENV} python /tmp/01-hello-world/hello.py"
     }
-}
-
+}   
